9ce8c02db49b408923c10b20735d48c4866f1fb0
David <d.azaro82@gmail.com>
2025-12-05 13:13:44 +0100

    Ejercicio 1 terminado

--- 3a/ej3a1/ej3a1_operations.py
+++ 3a/ej3a1/ej3a1_operations.py
@@ -3,19 +3,21 @@
 
 def add(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    return num_1 + num_2
 
 
 def subtract(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    return num_1 - num_2
 
 
 def multiply(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    return num_1 * num_2
 
 
 def divide(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    if num_2 == 0:
+        raise ZeroDivisionError("No se puede dividir por 0")
+    return num_1 / num_2
