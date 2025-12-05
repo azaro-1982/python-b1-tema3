9598bd56c2c66ed191c6ec7a36443fa7bcbe9d0b
David <d.azaro82@gmail.com>
2025-12-05 13:09:18 +0100

    Todos los ejercicios realizados

--- 3a/ej3a3/ej3a3_time_package/__init__.py
+++ 3a/ej3a3/ej3a3_time_package/__init__.py
@@ -1,2 +1,10 @@
 # write import for convert_date_format
 # write import for date_operations
+from . import convert_date_format
+from . import date_operations
+# He añadido el punto antes de importar los módulos para hacer referencia
+# al directorio actual
+__all__=[
+    "convert_date_format",
+    "date_operations"
+]
